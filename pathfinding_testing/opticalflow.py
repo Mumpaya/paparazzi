5684ec7ce6d48876611e741b2340ae66e87760b7
ruben <r.j.vanduuren@student.tudelft.nl>
2026-03-06 09:53:18 +0100

    path

--- pathfinding_testing/opticalflow.py
+++ pathfinding_testing/opticalflow.py
@@ -131,8 +131,12 @@ def draw_point_with_depth(frame, pt, z, fac=100):
 
 
 # 1. Setup Paths 20190121-142935
-folder_path = r"C:\Users\super\Downloads\AE4317_2019_datasets\AE4317_2019_datasets\cyberzoo_poles_panels_mats\20190121-142935"
-csv_path = r"C:\Users\super\Downloads\AE4317_2019_datasets\AE4317_2019_datasets\cyberzoo_poles_panels_mats\20190121-142943.csv"
+# folder_path = r"C:\Users\super\Downloads\AE4317_2019_datasets\AE4317_2019_datasets\cyberzoo_poles_panels_mats\20190121-142935"
+# csv_path = r"C:\Users\super\Downloads\AE4317_2019_datasets\AE4317_2019_datasets\cyberzoo_poles_panels_mats\20190121-142943.csv"
+
+# ruben laptop
+folder_path = r"/home/ruben/Downloads/AE4317_2019_datasets/cyberzoo_poles_panels_mats/20190121-142935"
+csv_path = r"/home/ruben/Downloads/AE4317_2019_datasets/cyberzoo_poles_panels_mats/20190121-142943.csv"
 
 # 2. Load and Prepare Data
 df = pd.read_csv(csv_path)
